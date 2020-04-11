9204329db33e73449c077201f4a67bd30cee9cb1
Juan Carlos <juancarlosenrique103@gmail.com>
2020-04-11 04:07:14 -0500

    Accordion terminad0 :)

---
+++ accordion/scripts/main.js
@@ -0,0 +1,15 @@
+
+function Accordion(element){
+    this.element= document.querySelector(element);
+    this.element.addEventListener('click', ()=>{
+        this.open();
+    })
+}
+
+Accordion.prototype.open = function(){
+    this.element.nextElementSibling.classList.toggle('is-active');
+} 
+
+let accordion1= new Accordion('.open_1');
+let accordion2= new Accordion('.open_2');
+let accordion3= new Accordion('.open_3');
